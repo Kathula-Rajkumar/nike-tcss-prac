8b53b8ad698e380d3b9f53544fbd3279839aa8b4
Kathula-Rajkumar <rajkumar@Rajkumars-MacBook-Air.local>
2025-08-19 23:35:16 +0530

    added some more components

---
+++ src/components/PopularProductCard.jsx
@@ -0,0 +1,27 @@
+import { star } from "../assets/icons"
+
+const PopularProductCard = ({imgURL, name, price}) => {
+  return (
+    <div className="flex flex-1 flex-col w-full max-sm:w-full">
+        <img 
+          src={imgURL}
+          alt={name}
+          className="w-[280px] h-[280px]"
+        />
+        <div className="mt-8 flex justify-start gap-2.5">
+            <img
+               src={star}
+               alt="rating"
+               width={24}
+               height={24}
+            />
+            <p className="font-montserrat text-xl text-slate-gray leading-normal">(4.5)</p>
+        </div>
+        
+            <h3 className="mt-2 text-2xl font-palanquin font-semibold leaidng-normal">{name}</h3>
+            <p className="mt-2 font-montserrat text-2xl leading-normal text-coral-red font-semibold">{price}</p>
+    </div>
+  )
+}
+
+export default PopularProductCard
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,10 +1,45 @@
+import { arrowRight } from "../assets/icons"
+import { offer } from "../assets/images"
+import Button from "../components/Buton"
 
 
 const SpecialOffer = () => {
   return (
-    <div>
-        SpecialOffer
-    </div>
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container ">
+      <div className="flex-1">
+          <img src={offer}
+          width={773}
+          height={687}
+          className="object-contain w-full" 
+          />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+       
+          <span className="text-coral-red"> Special</span>
+          <span> Offer</span> 
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Embark on a shopping journey that redefines your experience with unbeatable deals. Crafted for
+          durability, comfort, and effortless movement.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">Redefining activewear
+          with sleek design and lasting comfort. Designed to deliver unmatched
+          style, comfort, and performance.</p>
+        <div className="mt-11 w-auto flex flex-wrap gap-4">
+            <Button label="Shop now" 
+            iconURL={arrowRight}
+            />
+            <Button label="Learn more"
+            backgroundColor="bg-white"
+            borderColor="border-slate-gray"
+            textColor="text-slate-gray"
+
+            />
+        </div>
+      </div>
+        
+    </section>
   )
 }
 
